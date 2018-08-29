f89082547e1f20e6b03efea314505c3dbbf726ee
a-oliveira <dessaschweitzer@gmail.com>
2018-08-29 15:15:29 -0500

    Exercicio 2 completo

--- exe2-aula1.cpp
+++ exe2-aula1.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <GL/glut.h>
-#include "GL/glui.h"
+#include <GL/glui.h>
 #include <math.h>
 void display 	(void);
 void init 		(void);
@@ -14,8 +14,9 @@ int main(int argc, char *argv[])
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-	glutInitWindowSize(500,500);
+	glutInitWindowSize(600,600);
 	glutInitWindowPosition(100,100);
+
 	// Cria janela principal
 	janela_principal = glutCreateWindow("Seno"); 
 	glutDisplayFunc(display);
@@ -45,51 +46,67 @@ int main(int argc, char *argv[])
 void init (void)
 {
 	glClearColor(0.0, 0.0, 0.0, 0.0);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(-10.0, 390, -2, 2, -1.0, 1.0);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
 }
 
 void display(void)
 {
 	glClear (GL_COLOR_BUFFER_BIT);
+
+	// Define área de visualização
+	// p/  exibir somente a segunda metade do gráfico
+	glViewport((int) -1, (int)0, 600, 600);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-10.0, 390, -2, 2, -1.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
 	plano();
 	grafico();
 	glutSwapBuffers();
+	glutPostRedisplay();
 }
 
 void plano(void)
 {
 	glBegin(GL_LINES);
-		glColor3f (0.5, 0.0, 1.0);
+		glColor3f (1.0, 1.0, 1.0);
 		glVertex3f (-10.0, 0.0, 0.5);
 		glVertex3f (390.0, 0.0, 0.5);
 
-		glColor3f (0.5, 0.7, 0.2);
+		glColor3f (1.0, 1.0, 1.0);
 		glVertex3f (0.0, -2.0, 0.5);
 		glVertex3f (0.0, 2.0, 0.5);
     glEnd();
+
+    glColor3f(1.0,0.0,0.0);
+    glPointSize(10.0f);
+	glBegin(GL_POINTS);
+		glVertex3f (0.0, 0.0, 0.5);
+	glEnd();
 }
 
 void grafico(void)
 {
-	float pi = 3.1415;
-	if (opcao == 0)
+	float y, pi = 3.1415;
+	int i;
+	if (opcao == 0) i = 360/6;
+	else if (opcao == 1) i = 360/12;
+	else if (opcao == 2) i = 360/180;
+
+	for (int x = 0; x < 360; x = x+i)
 	{
+		glColor3f(0.0,1.0,0.6);
 		glBegin(GL_LINE_STRIP);
-			glColor3f(1.0,0.0,0.0);
-			glVertex3f(0.0, sin(0), 0.5);
-			glVertex3f(60.0,sin(60*pi/180),0.5);
-			//glVertex3f(120.0,sin(120*pi/180),0.5);
-			//glVertex3f(180.0, sin(180*pi/180), 0.5);
-			//glVertex3f(240.0,sin(240*pi/180),0.5);
-			//glVertex3f(300.0,sin(300*pi/180),0.5);
+
+			y	 = sin(x*pi/180);
+
+			glVertex3f(x, y, 0.5);
+			glVertex3f(x+i,sin((x+i)*pi/180),0.5);
+
 		glEnd();
 	}
-	else if (opcao == 1);
-	else if (opcao == 2);
 	
 	
 }
