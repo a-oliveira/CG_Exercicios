e4a7d6c3f0915dd663d68cc6119b61632b9edf5d
a-oliveira <dessaschweitzer@gmail.com>
2018-08-31 13:49:38 -0500

    Completo

--- exe2-aula2.cpp
+++ exe2-aula2.cpp
@@ -13,9 +13,15 @@
 #include<iostream>
 using namespace std;
 
-float angle = 0, scale = 1.0;
-float xtrans = 0, ytrans = 0, ztrans = 0;
-int enableMenu = 0;
+float angle       = 0, scale  = 1.0;
+float temp1       = 0;
+float temp2       = 1.0;
+float xtrans      = 0, ytrans = 0, ztrans = 0;
+float x           = 0, y = 0, z = 0;
+int enableMenu    = 0;
+float desiredFPS  = 15;
+float var         = 0.0f;
+float dir         = 1.0f;
 
 void display(void);
 void init (void);
@@ -31,12 +37,11 @@ int main(int argc, char** argv)
    glutInitWindowSize (300, 300);
    glutInitWindowPosition (100, 100);
    glutCreateWindow ("hello");
-   glutMouseFunc( mouse );
    glutIdleFunc(idle);
+   glutMouseFunc( mouse );
    init ();
    printf("Posicione as janelas e clique na janela do OpenGL para habilitar o menu.\n");
    glutDisplayFunc(display);
-
    glutMainLoop();
 
    return 0;
@@ -47,6 +52,8 @@ void mouse(int button, int state, int x, int y)
 {
    if ( button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
       enableMenu = 1;
+   else
+      enableMenu = 0;
 }
 
 void showMenu()
@@ -66,17 +73,17 @@ void showMenu()
       case 1:
          printf("\n\nInforme o vetor de translacao (entre -100.0 e 100.0)");
          printf("\nX : ");
-         scanf("%f", &xtrans);
+         scanf("%f", &x);
          printf("Y : ");
-         scanf("%f", &ytrans);
+         scanf("%f", &y);
       break;
       case 2:
          printf("\n\nInforme o angulo de rotacao (em graus): ");
-         scanf("%f", &angle);
+         scanf("%f", &temp1);
       break;
       case 3:
          printf("\n\nInforme o fator de escala: ");
-         scanf("%f", &scale);
+         scanf("%f", &temp2);
       break;
       case 4:
          exit(1);
@@ -112,24 +119,49 @@ void display(void)
       glScalef(scale, scale, scale);
       glutWireCube(10);
    glPopMatrix();
-
+   
    glutSwapBuffers ();
-   glutPostRedisplay();
-
    if(enableMenu) showMenu();
+
+   
 }
 void idle (void)
 {
-   glColor3f (0.0, 0.0, 1.0);
-   glTranslatef(0, 0, 0.0);
-   glRotatef(angle, 0.0, 0.0, 1.0);
-   glScalef(scale, scale, scale);
-   glutWireCube(10);
+    float t, desiredFrameTime, frameTime;
+    static float tLast = 0.0;
+    // Get elapsed time
+    t = glutGet(GLUT_ELAPSED_TIME);
+    // convert milliseconds to seconds
+    t /= 1000.0;
 
+    // Calculate frame time
+    frameTime = t - tLast;
+    // Calculate desired frame time
+    desiredFrameTime = 1.0 / (float) (desiredFPS);
 
-   cout << "teste";
-    glutPostRedisplay();
-   glutSwapBuffers ();
+    // Check if the desired frame time was achieved. If not, skip animation.
+    if( frameTime <= desiredFrameTime)
+        return;
+
+    /*
+     *UPDATE ANIMATION VARIABLES
+     */
+
+      if(xtrans < x) xtrans += dir * 1;
+      else if (xtrans > x) xtrans += dir * -1;
+
+      if(ytrans < y) ytrans += dir * 1;
+      else if (ytrans > y) ytrans += dir * -1;
+
+      if(angle < temp1) angle += dir * 1;
+      else if (angle > temp1) angle += dir * -1;
+
+      if(scale < temp2) scale += dir * 1;
+      else if (scale > temp2) scale += dir * -1; 
+
+      glutPostRedisplay(); 
+    /* Update tLast for next time, using static local variable */
+    tLast = t;
 }
 void init (void)
 {
