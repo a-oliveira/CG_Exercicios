143c529c4e346ecabe8b93b6e95f89c80d535f37
a-oliveira <dessaschweitzer@gmail.com>
2018-09-07 12:14:29 -0300

    Garra bugada

--- exe2-aula3.cpp
+++ exe2-aula3.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 float shoulder = 0.0f, elbow = 0.0f, wrist = 0.0f;
+float temp1 = 80, temp2 = 110;
 
 int selected = 1;
 
@@ -44,26 +45,47 @@ void desenhaEixos()
 }
 void garra (void)
 {
-   glPushMatrix();
+	/* Transformações são cumulativas
+	os valores do segundo cubo levam
+	em consideração os do primeiro */
 
-      glTranslatef(1.3, 0.3, 0.0); /* aplicado a todos os blocos */
-      glRotatef(45.0, 0.0, 0.0, 1.0);
-      glColor3f(1.0f, 0.0f, 0.0f);
-      glPushMatrix();
-         glScalef(1.4,0.5,0.5);
-         glutSolidCube(0.8);
-      glPopMatrix();
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glTranslatef(1.2,0.8, 0.3);
+    glRotatef(70.0, 0.0, 0.0, 1.0); // rotaciona em relação à origem
 
-     // glColor3f(0.0f, 1.0f, 0.0f);
-      glTranslatef(-1.0, -0.3, 0.0); /* altura, profundidade, largura */
+    /* Base da garra */
+    glPushMatrix();
+        glScalef(1.0,0.3,0.3);
+        glutSolidCube(1.3);
+    glPopMatrix();
 
-      glRotatef(65.0, 0.0, 0.0, 1.0);
-      glPushMatrix();
-         glScalef(1.4,0.5,0.5);
-         glutSolidCube(0.8);
-      glPopMatrix();
+    glTranslatef(-1.2,-0.8, 0.0);
+    glRotatef(250.0, 0.0, 0.0, 1.0);
+    
+    glPushMatrix();
+        glScalef(1.0,0.3,0.3);
+        glutSolidCube(1.3);
+    glPopMatrix();
 
-   glPopMatrix();
+    /* * * * * * * * * */
+
+    //glColor3f(0.0f, 1.0f, 0.0f);
+    glTranslatef(0.5,0.3, 0.0);
+    glRotatef(temp1, 0.0, 0.0, 1.0);
+    
+    glPushMatrix();
+        glScalef(0.8,0.3,0.3);
+        glutSolidCube(1.3);
+    glPopMatrix();
+
+    //glColor3f(0.0f, 0.0f, 1.0f);
+    glTranslatef(0.9,1.7, 0.0);
+    glRotatef(temp2, 0.0, 0.0, 1.0);
+   
+    glPushMatrix();
+        glScalef(0.8,0.3,0.3);
+        glutSolidCube(1.3);
+    glPopMatrix();
 }
 
 void display(void)
@@ -78,7 +100,7 @@ void display(void)
 		glTranslatef (1.0, 0.0, 0.0); // Translada 1.0, pois o braço terá 2.0 de comprimento
 		glPushMatrix();
 			glScalef (2.0, 0.5, 0.5);
-			glutSolidCube (1.0);
+			glutSolidCube (1.2);
 		glPopMatrix();
 
 		glTranslatef (1.0, 0.0, 0.0); // origem posicionada no cotovelo
@@ -86,7 +108,7 @@ void display(void)
 		glTranslatef (1.0, 0.0, 0.0); // posiciona antebraço de tamanho 2 na posição correta
 		glPushMatrix();
 			glScalef (2.0, 0.5, 0.5);
-			glutSolidCube (1.0);
+			glutSolidCube (1.2);
 		glPopMatrix();
 
       glTranslatef (1.0, 0.0, 0.0); /* desloca mais à frente */
@@ -94,7 +116,7 @@ void display(void)
       glTranslatef(1.0, 0.0, 0.0); /* desloca pra frente */
       glPushMatrix();
          glScalef(2.0,0.5,0.5); /* cria na origem */
-         glutSolidCube(1.0);
+         glutSolidCube(1.2);
       glPopMatrix();
 
       garra();
@@ -109,7 +131,7 @@ void reshape (int w, int h)
 
    glMatrixMode (GL_PROJECTION);
    glLoadIdentity ();
-   gluPerspective(90.0, (GLfloat) w/(GLfloat) h, 1.0, 200.0);
+   gluPerspective(100.0, (GLfloat) w/(GLfloat) h, 1.0, 200.0);
 
    glMatrixMode(GL_MODELVIEW);
    glLoadIdentity ();
@@ -145,12 +167,22 @@ void specialKeys(int key, int x, int y)
       case GLUT_KEY_LEFT:
 			if(selected == 1) shoulder = ((int) shoulder + 5) % 360;
 			if(selected == 2) elbow    = ((int) elbow + 5) % 360;
-         if(selected == 3) wrist    = ((int) wrist + 5) % 360;
+         if(selected == 3) wrist       = ((int) wrist + 5) % 360;
       break;
       case GLUT_KEY_RIGHT:
 			if(selected == 1) shoulder = ((int) shoulder - 5) % 360;
 			if(selected == 2) elbow    = ((int) elbow - 5) % 360;
-         if(selected == 3) wrist    = ((int) wrist - 5) % 360;
+         if(selected == 3) wrist       = ((int) wrist - 5) % 360;
+      break;
+
+      case GLUT_KEY_UP:
+      		temp1 = temp1 - 5;
+      		temp2 = temp2 + 5;
+      break;
+
+      case GLUT_KEY_DOWN:
+      		temp1 = temp1 + 5;
+      		temp2 = temp2 - 5;
       break;
    }
    glutPostRedisplay();
